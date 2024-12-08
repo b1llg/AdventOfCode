7de571318f3cc5f8a32b696af1d5a5bbf424a2ed
Billy Guy <guy.billy@outlook.com>
2024-12-07 20:00:26 -0500

    dev day2 part2

--- 2024/cxx/Day2/main.cpp
+++ 2024/cxx/Day2/main.cpp
@@ -4,6 +4,11 @@
 #include <sstream>
 #include <vector>
 
+bool is_respecting_increase_decrease_rule(int n)
+{
+    return (n == 0 || abs(n) > 3) ? true : false;
+}
+
 bool change_by_n(std::vector<int> seq)
 {
     /*
@@ -12,13 +17,13 @@ bool change_by_n(std::vector<int> seq)
 
     int n = seq.at(1) - seq.at(0);
 
-    if (n != -2 && n != -1 && n != 1 && n != 1 && n != 3)
+    if (is_respecting_increase_decrease_rule(n))
         return false; // means that it does not respect the rules
 
     // Now check that it is either increasing or decreasing
     bool increase = n > 0 ? true : false;
 
-    for (int i = 1; i < seq.size(); i++)
+    for (long unsigned int i = 1; i < seq.size(); i++)
     {
         // 1) check the change of direction
         int ni = seq.at(i) - seq.at(i-1);
@@ -30,11 +35,46 @@ bool change_by_n(std::vector<int> seq)
 
         // 2) check the increase size
 
-        if (ni != -2 && ni != -1 && ni != 1 && ni != 1 && ni != 3)
+        if (is_respecting_increase_decrease_rule(ni))
             return false; // means that it does not respect the rules 
     }
 
     return true; // if it gets here it means it as respected all condition
+
+    // means increase was
+}
+
+std::vector<int>::iterator change_by_n2(std::vector<int> seq)
+{
+    /*
+            Determine n, either -2,-1,1,2 or 3
+    */
+
+    int n = *(seq.begin() + 1) - *(seq.begin());
+
+    if (is_respecting_increase_decrease_rule(n))
+        return seq.begin(); // means that it does not respect the rules
+
+    // Now check that it is either increasing or decreasing
+    bool increase = n > 0 ? true : false;
+
+    for (auto viter = seq.begin() + 1; viter != seq.end(); ++viter)
+    {
+        // 1) check the change of direction
+        int ni = *viter - *(viter-1);
+
+        bool increase2 = ni > 0 ? true : false;
+
+        if (increase !=  increase2)
+            return viter-1; // quit because its changing direction
+
+        // 2) check the increase size
+
+        if (is_respecting_increase_decrease_rule(ni))
+            return viter-1; // means that it does not respect the rules 
+    }
+
+    return seq.end(); // if it gets here it means it as respected all condition
 
     // means increase was
 }
@@ -62,12 +102,16 @@ int main()
             vint.push_back(val);
 
         // Remove id, use the change_by_n function
-        vint.erase(vint.begin());
+        // vint.erase(vint.begin());
 
-        if (change_by_n(vint))
+        if (change_by_n2(vint) == vint.end())
         {
             safe++;
             std::cout << "line#:" << line_num << std::endl;
+        }
+        else // try one more time returning the seq without the problematic level
+        {
+            std::cout << "damper success" << std::endl;
         }
 
         line_num++;
